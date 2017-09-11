22d006d9d8585cfab25cd81cc0b79bfcc3406aed
Menno Finlay-Smits <inbox@menno.io>
2017-09-11 15:13:35 +1200

    Timestamps when logging & smarter sequence checking

--- lepton_packets.c
+++ lepton_packets.c
@@ -24,6 +24,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <stdarg.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -34,6 +35,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <limits.h>
+#include <time.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -71,6 +73,27 @@ static int last_good_count = 0;
 
 static uint8_t tx[TRANSFER_SIZE] = {0, };
 
+void debug(char *fmt, ...) {
+    time_t t = time(NULL);
+    struct tm *ti = localtime(&t);
+    va_list ap;
+    va_start(ap, fmt);
+    printf("%02d:%02d:%02d: ", ti->tm_hour, ti->tm_min, ti->tm_sec);
+    vprintf(fmt, ap);
+    printf("\n");
+    va_end(ap);
+}
+
+int is_valid_seq(int prev, int now) {
+    if (now == 0 && prev == 60) {
+        return 1;
+    }
+    if (now == prev + 1) {
+        return 1;
+    }
+    return 0;
+}
+
 int transfer(int fd)
 {
     int ret;
@@ -97,55 +120,62 @@ int transfer(int fd)
     }
 
     for (i = 0; i < PACKETS_PER_READ; i++) {
-	packet = &rx[i * VOSPI_PACKET_SIZE];
-	header = packet[1] | packet[0] << 8;
+        packet = &rx[i * VOSPI_PACKET_SIZE];
+        header = packet[1] | packet[0] << 8;
 
-	/* XXX if first bit of buffer is set, trigger a reset */
-    
-	if ((header & 0x0F00) == 0x0F00) {
-	    state = STATE_INVALID;
-	    invalid++;
-	} else {
-	    pkt_num = header & 0x0FFF;
-	    if (pkt_num > 60) {
-		state = STATE_INVALID_PKTNUM;
-		invalid_pktnum++;
-		return 1;
-	    } else {
-		state = STATE_VALID;
-		if (pkt_num == 20) {
-		    printf("p %d: %d\n", pkt_num, packet[0] >> 4);
-		}
-	    }
-	}
+        /* XXX if first bit of buffer is set, trigger a reset */
 
-	if (state != last_state) {
-	    switch (last_state) {
-	    case STATE_VALID:
-		printf("saw packets: 0-%d\n", last_pkt_num);
-		last_pkt_num = -1;
-		break;
-	    case STATE_INVALID:
-		printf("invalid: %d\n", invalid);
-		invalid = 0;
-		break;
-	    case STATE_INVALID_PKTNUM:
-		printf("invalid packet numbers: %d\n", invalid_pktnum);
-		invalid_pktnum = 0;
-		break;
-	    }
-	    last_state = state;
-	}
+        if ((header & 0x0F00) == 0x0F00) {
+            state = STATE_INVALID;
+            invalid++;
+        } else {
+            pkt_num = header & 0x0FFF;
+            if (pkt_num > 60) {
+                debug("invalid packet number: %d", pkt_num);
+                state = STATE_INVALID_PKTNUM;
+                invalid_pktnum++;
+                return 1;
+            } else {
+                state = STATE_VALID;
+                if (pkt_num == 20) {
+                    debug("p %d: %d", pkt_num, packet[0] >> 4);
+                }
+            }
+        }
 
-	if (state == STATE_VALID) {
-	    if (pkt_num != last_pkt_num+1) {
-		if (last_pkt_num != -1) {
-		    printf("saw packets: 0-%d\n", last_pkt_num);
-		}
-	        return 1;
-	    }
-	    last_pkt_num = pkt_num;
-	}
+        if (state != last_state) {
+            switch (last_state) {
+            case STATE_VALID:
+                debug("saw packets: 0-%d", last_pkt_num);
+                last_pkt_num = -1;
+                break;
+            case STATE_INVALID:
+                debug("invalid: %d", invalid);
+                invalid = 0;
+                break;
+            case STATE_INVALID_PKTNUM:
+                debug("invalid packet numbers: %d", invalid_pktnum);
+                invalid_pktnum = 0;
+                break;
+            }
+            last_state = state;
+        }
+
+        if (state == STATE_VALID) {
+            if (pkt_num == 0 && last_pkt_num == 0) {
+                // XXX end of read?
+                debug("saw pkt 0 twice (%02x %02x %02x %02x)", packet[0], packet[1], packet[2], packet[3]);
+                return 0;
+            }
+            if (!is_valid_seq(last_pkt_num, pkt_num)) {
+                if (last_pkt_num != -1) {
+                    debug("saw packets: 0-%d", last_pkt_num);
+                }
+                debug("pkts out of sequence: %d, previous %d", pkt_num, last_pkt_num);
+                return 1;
+            }
+            last_pkt_num = pkt_num;
+        }
     }
 
     return 0;
@@ -196,15 +226,16 @@ int spi_init() {
         pabort("can't get max speed hz");
     }
 
-    printf("spi mode: %d\n", mode);
-    printf("bits per word: %d\n", bits);
-    printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
-    printf("size per read: %d\n", TRANSFER_SIZE);
+    debug("spi mode: %d", mode);
+    debug("bits per word: %d", bits);
+    debug("max speed: %d Hz (%d KHz)", speed, speed/1000);
+    debug("size per read: %d", TRANSFER_SIZE);
 
     return fd;
 }
 
 int reset(int fd) {
+    debug("reset!");
     close(fd);
 
     invalid = 0;
@@ -225,10 +256,10 @@ int main(int argc, char *argv[])
     int need_reset;
 
     while (1 == 1) {
-	need_reset = transfer(fd);
-	if (need_reset) {
-	    reset(fd);
-	}
+        need_reset = transfer(fd);
+        if (need_reset) {
+            reset(fd);
+        }
     }
 
     close(fd);
